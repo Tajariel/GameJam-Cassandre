5fe406b1242f870047a85b87df5a2c9986099370
Ambaud Marc <62051015+Tajariel@users.noreply.github.com>
2022-11-22 17:24:23 +0100

    Added confirmation to Grabber script
    When right click when object is moving object go back to starting position
    when left click while moving object enter confirmation state.
    When in confirmation state left click put the object at this position, right click put it back to starting position

--- GameJam Cassandre/Assets/Scripts/Grabber.cs
+++ GameJam Cassandre/Assets/Scripts/Grabber.cs
@@ -7,15 +7,26 @@ public class Grabber : MonoBehaviour
 {
 
     private GameObject selectedObject;
+    public bool confirmationState = false;
     //Change this value to change size of grid
     public float gridSize = 10f;
+    public Vector3 startPosition;
+    public Vector3 currentPos;
     
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButtonDown(0)){
+            
+            //When object is in confirmation and left click is used accept movement of object
+            if(confirmationState == true){
+                selectedObject = null;
+                confirmationState = false;
+                print("confirmé");
+            }
             //When left click when nothing selected, grab object
+
             if(selectedObject == null){
                 RaycastHit hit = CastRay();
 
@@ -23,21 +34,45 @@ public class Grabber : MonoBehaviour
                     if(!hit.collider.CompareTag("drag")){
                         return;
                     }
+                    
                     selectedObject = hit.collider.gameObject;
+                    startPosition = selectedObject.transform.position;
+                    print("grabbed");
                     //Cursor.visible = false;
                 }
             }else{
                 //when left click when object is selected, drop object where mouse is
+                print(selectedObject);
                 Vector3 position = new Vector3(Input.mousePosition.x,Input.mousePosition.y, Camera.main.WorldToScreenPoint(selectedObject.transform.position).z);
                 Vector3 worldPosition = Camera.main.ScreenToWorldPoint(position);
                 selectedObject.transform.position = new Vector3(RoundToNearestGrid(worldPosition.x),0f,RoundToNearestGrid(worldPosition.z));
-                
-                selectedObject = null;
+                currentPos = selectedObject.transform.position;
+                confirmationState = true;
+                print(selectedObject);
+                print("posé en attente de confirmation");
                 //Cursor.visible = true;
             }
         }
+
+        //Cancel potential object move by left clicking
+        if(Input.GetMouseButtonDown(1)){
+
+            if(confirmationState == true){
+                selectedObject.transform.position = startPosition;
+                selectedObject = null;
+                confirmationState = false;
+                print("annulé en confirmation");
+            }else if(selectedObject != null){
+                selectedObject.transform.position = startPosition;
+                selectedObject = null;
+                print(selectedObject);
+                print("annulé en mouvement");
+            }
+            
+        }
+
         //When object is selected move object with mouse
-        if(selectedObject != null){
+        if((selectedObject != null) && (confirmationState== false)){
             Vector3 position = new Vector3(Input.mousePosition.x,Input.mousePosition.y, Camera.main.WorldToScreenPoint(selectedObject.transform.position).z);
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(position);
             selectedObject.transform.position = new Vector3(RoundToNearestGrid(worldPosition.x),1f,RoundToNearestGrid(worldPosition.z));
@@ -71,7 +106,6 @@ public class Grabber : MonoBehaviour
         if (xDiff>(gridSize/2)){
             pos+=gridSize;
         }
-        print("pos "+pos+"/gridSize "+gridSize+"/xDiff "+xDiff);
         return pos;
     }
 }
---
+++ GameJam Cassandre/Assets/Scripts/MoveCounter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCounter : MonoBehaviour 
+{
+    public Grabber objectGrabbed;
+    private int moveCount;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
