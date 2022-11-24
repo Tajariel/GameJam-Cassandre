ae6e40305cb4a343b5cf5df7d05172dc6eebdd28
Ambaud Marc <62051015+Tajariel@users.noreply.github.com>
2022-11-24 16:06:54 +0100

    Completion of UI_Counter Script

--- GameJam Cassandre/Assets/Scripts/UI_Counter.cs
+++ GameJam Cassandre/Assets/Scripts/UI_Counter.cs
@@ -1,20 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UI_Counter : MonoBehaviour
 {
 
     public List<GameObject> listDragableObjects;
-    // Start is called before the first frame update
-    void Start()
+    public Text textCounter;
+    private MoveCounter selectedObj;
+
+    public void UpdateCounter()
     {
-        
+        float totalMoves = 0;
+            for(int i=0;i<listDragableObjects.Count;++i)
+            {
+                MoveCounter mc = listDragableObjects[i].GetComponent<MoveCounter>();
+                if(mc)
+                {
+                    totalMoves += mc.moveCount;
+                    print(listDragableObjects[i].GetComponent<MoveCounter>().moveCount);
+                    print (totalMoves);
+                }
+                else
+                {
+                    Debug.LogWarning("TA MERE");
+                }
+            }
+            //counter.SetText(totalMoves.ToString());
+            textCounter.text = totalMoves.ToString();
     }
 
-    // Update is called once per frame
+    // Start is called before the first frame update
+    void Awake()
+    {
+        textCounter = GetComponent<Text>();
+    }
+
+    /*// Update is called once per frame
     void Update()
     {
-        
-    }
+       // Debug.Log(textCounter.text);
+        if(Input.GetMouseButtonDown(0)||Input.GetMouseButtonDown(1))
+        {
+            
+        }
+    }*/
 }
