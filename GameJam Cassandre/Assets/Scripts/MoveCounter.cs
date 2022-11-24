ed8be2d7b9ecbfa0ff331431062cc21f9f5e434f
Ambaud Marc <62051015+Tajariel@users.noreply.github.com>
2022-11-24 14:43:47 +0100

    Added People Tag

--- GameJam Cassandre/Assets/Scripts/MoveCounter.cs
+++ GameJam Cassandre/Assets/Scripts/MoveCounter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MoveCounter : MonoBehaviour 
 {
@@ -42,7 +43,6 @@ public class MoveCounter : MonoBehaviour
         }else if(objectGrabbed.confirmationState==false){
             moveCount+=TotalDistanceMoved();
         }
-        print(moveCount);
     }
     
 
